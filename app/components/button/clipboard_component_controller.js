6415d1ecee54983df5a7e0f89dc75d0af614bdca
ptphong <pkphung2001@gmail.com>
2023-07-01 18:02:28 +0700

    update

--- app/components/button/clipboard_component_controller.js
+++ app/components/button/clipboard_component_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ['source']
+  static values = {
+    sourceNodeName: String
+  }
 
   initialize() {
     this.element.firstElementChild.setAttribute('data-button--clipboard-component-target', 'source')
+    this.sourceNodeName = this.element.firstElementChild.nodeName
   }
   copy(event) {
     event.preventDefault()
-    navigator.clipboard.writeText(this.sourceTarget.innerText)
+    switch (this.sourceNodeName) {
+      case "INPUT":
+        navigator.clipboard.writeText(this.sourceTarget.value);
+        break;
+      default:
+        navigator.clipboard.writeText(this.sourceTarget.innerText);
+        break;
+    }
   }
 }
