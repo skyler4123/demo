c8c0d0f560bf4d66260fa02260f140a629a38562
ptphong <pkphung2001@gmail.com>
2023-07-07 00:25:18 +0700

    update

--- app/components/rich_text/trix_component_controller.js
+++ app/components/rich_text/trix_component_controller.js
@@ -1,13 +1,8 @@
+import Trix from "trix";
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ['table', 'header', 'body', 'row']
   connect() {
-
-  }
-
-  typeOfCell(cell) {
-    // type = 
-    return type
+    console.log("Hello, Stimulus!", this.element);
   }
 }
